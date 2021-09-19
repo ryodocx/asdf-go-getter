d8afc82e094cbbb87bc83a75f13c81a59b1cc320
ryodocx <email@ryodocx.net>
2021-09-19 11:32:00 +0900

    fix

--- lib/utils.bash
+++ lib/utils.bash
@@ -25,7 +25,7 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/v1.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
